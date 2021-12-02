a807384251c7cbb442851c762fc2c9100b9bdd9f
遥香 鈴木 <190455@jc-21.jp>
2021-12-02 14:24:26 +0900

    写真

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -24,26 +24,65 @@ namespace R03BMI
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            
-          
 
-            h=double.Parse(height.Text);
-            w=double.Parse(weight.Text);
+            try 
+            { 
+                string height=heighit.Text;
+                string weight=weighi.Text;
 
-            if (h>100)
+                double height2=double.Parse(height);
+                double weight2=double.Parse(weight);
+
+            if (height2>=10)
             {
-                h=h/100;
+                height2=height2/100;
             }
 
-            if (w>1000)
+            if wheight2>=1000)
             {
-                w=w/1000;
+                weight2=weight2/1000;
             }
-          
+
+
+
+            double BMI=weight2/height2*height2;
+            double BMI2=Math.Round(BMI,1);
+                string status=null;
+
+            if (BMI22>=18.5)
+                {
+                    status="低体重(痩せ)";
+                 }
+            else if(BMI2<25){
+                    
+                    status="普通体重";
+                }
+             else if(BMI2<30){
+
+                    status="肥満 (1度)";
+                }
+             else if(BMI2<35){
+
+                    status="肥満 (2度)";
+                }
+             else if(BMI2<40){
+                    status="肥満 (3度)";
+                }
+            else {
+                    status="肥満 (4度)";
+                }
            
-            bmi=w/(h*h);
-            result.Text=(Math.Round(bmi,1,MidpointRounding.AwayFromZero)).ToString;
-          
+            result.Text="BMI値は"+BMI2+",状態は"+status+"です。";
+
+
+                catch(FormatException ex)
+                {
+                    result.Text=ex.Message;
+                    Console.WriteLine(ex.Message);
+                }
+
+
+            }
         }
     }
 }
