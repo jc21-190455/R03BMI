7e7ae284ec14f6869c64e00af1426ca6f981efbe
遥香 鈴木 <190455@jc-21.jp>
2021-12-02 14:30:50 +0900

    a

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -25,64 +25,71 @@ namespace R03BMI
         private void Button_Clicked(object sender, EventArgs e)
         {
 
-            try 
-            { 
-                string height=heighit.Text;
-                string weight=weighi.Text;
-
-                double height2=double.Parse(height);
-                double weight2=double.Parse(weight);
-
-            if (height2>=10)
+            try
             {
-                height2=height2/100;
+                string height = heighit.Text;
+                string weight = weighit.Text;
+
+                double height2 = double.Parse(height);
+                double weight2 = double.Parse(weight);
+
+                if (height2 >= 10)
+                {
+                    height2 = height2 / 100;
+                }
+
+                if (weight2 >= 1000)
+                {
+                    weight2 = weight2 / 1000;
+                }
+
+
+
+                double BMI = weight2 / height2 * height2;
+                double BMI2 = Math.Round(BMI, 1);
+                string status = null;
+
+                if (BMI2 >= 18.5)
+                {
+                    status = "低体重(痩せ)";
+                }
+                else if (BMI2 < 25)
+                {
+
+                    status = "普通体重";
+                }
+                else if (BMI2 < 30)
+                {
+
+                    status = "肥満 (1度)";
+                }
+                else if (BMI2 < 35)
+                {
+
+                    status = "肥満 (2度)";
+                }
+                else if (BMI2 < 40)
+                {
+                    status = "肥満 (3度)";
+                }
+                else
+                {
+                    status = "肥満 (4度)";
+                }
+
+                result.Text = "BMI値は" + BMI2 + ",状態は" + status + "です。";
             }
 
-            if wheight2>=1000)
+
+            catch (FormatException ex)
             {
-                weight2=weight2/1000;
+                result.Text = ex.Message;
+                Console.WriteLine(ex.Message);
             }
-
-
-
-            double BMI=weight2/height2*height2;
-            double BMI2=Math.Round(BMI,1);
-                string status=null;
-
-            if (BMI22>=18.5)
-                {
-                    status="低体重(痩せ)";
-                 }
-            else if(BMI2<25){
-                    
-                    status="普通体重";
-                }
-             else if(BMI2<30){
-
-                    status="肥満 (1度)";
-                }
-             else if(BMI2<35){
-
-                    status="肥満 (2度)";
-                }
-             else if(BMI2<40){
-                    status="肥満 (3度)";
-                }
-            else {
-                    status="肥満 (4度)";
-                }
-           
-            result.Text="BMI値は"+BMI2+",状態は"+status+"です。";
-
-
-                catch(FormatException ex)
-                {
-                    result.Text=ex.Message;
-                    Console.WriteLine(ex.Message);
-                }
 
 
             }
         }
     }
-}
+
+
